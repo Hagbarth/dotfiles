a9b9cab6d4a51634dbc79f65f0d23fccf2adb163
Emil Rasmussen <emil@pento.dk>
2021-07-19 23:34:42 +0200

    wip

--- .zshrc
+++ .zshrc
@@ -1,11 +1,14 @@
 export ZSH_CUSTOM='/Users/hagbarth/.zsh_custom'
 
+export PENTO_DIR='/Users/hagbarth/pento/Pento'
+
 # Setup Google Cloud
 source '/Users/hagbarth/google-cloud-sdk/path.zsh.inc'
 source '/Users/hagbarth/google-cloud-sdk/completion.zsh.inc'
 
 alias pentodev="cd /Users/hagbarth/Developer/Go/src/gitlab.com/pentoapp/"
 export GCLOUD_CONFIG=$HOME/.config/gcloud
+export CLOUDSDK_PYTHON=python3
 
 # Setup Kubernetes
 export KUBE_EDITOR=vim
@@ -14,9 +17,13 @@ alias k=kubectl
 alias kprod="k config use-context gke_pentoapp_europe-west1-c_eu-prod"
 alias kfun="k config use-context gke_pentoapp_europe-west1-c_eu-fun"
 
+# Brew
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/hagbarth/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Go
 export GOPATH='/Users/hagbarth/Developer/Go'
-export GOROOT='/usr/local/go'
+export GOROOT='/opt/homebrew/opt/go'
 export PATH=$PATH:$GOROOT/bin
 export PATH=$PATH:$(go env GOPATH)/bin
 
@@ -34,13 +41,11 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 # Theme
+fpath+=$HOME/.zsh/pure
 ZSH_THEME=""
 PURE_PROMPT_SYMBOL="🦄 - ❯"
 autoload -U promptinit; promptinit
 prompt pure
-
-# Install scripts
-cp ~/.dotfiles/scripts/* /usr/local/bin
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/hagbarth/google-cloud-sdk/path.zsh.inc' ]; then source '/Users/hagbarth/google-cloud-sdk/path.zsh.inc'; fi
--- install.sh
+++ install.sh
@@ -5,28 +5,23 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 #Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/hagbarth/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install node js
 brew install node
+
 
 # Install Go
 brew install go
 
 # Install Google Cloud SDK
-url=https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-212.0.0-darwin-x86_64.tar.gz
-wantedsum=ea1bf841892dc89dd24685e281d5c301bf8a764eb41b595ca06279088be4afad
-outputfile=gcsdk.tar.gz
-echo "Downloading Google Cloud SDK..."
-curl -L -v  $url -o $outputfile 2>> /dev/null
-calcsum=$(shasum -a 256 $outputfile | awk '{print $1}')
-
-if [ "$calcsum" = "$wantedsum" ]; then
-  echo "Extracting..."
-  tar -xvzf $outputfile
-fi
-rm -rf $outputfile
+brew install pyenv
+pyenv install 3.9.1
+pyenv global 3.9.1
+export CLOUDSDK_PYTHON=python3
+curl https://sdk.cloud.google.com | bash
 ./google-cloud-sdk/install.sh
-
 
 # Install oh my zsh and set it as default shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -41,5 +36,10 @@ ln -sfv ~/.dotfiles/.vimrc ~
 
 #Theme
 npm install --global pure-prompt
+
+
+# Install scripts
+chmod +x ~/.dotfiles/scripts/*
+sudo cp ~/.dotfiles/scripts/* /usr/local/bin
 
 source ~/.zshrc
